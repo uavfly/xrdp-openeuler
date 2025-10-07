1fe137244996fb9b73ce069a10ed07b04ba6fabb
StrangeFly <116087836+uavfly@users.noreply.github.com>
2025-10-07 18:47:48 +0800

    Update something.

--- env.sh
+++ env.sh
@@ -1,1 +1,4 @@
 yum install -y libxkbfile-devel nasm tigervnc-server tigervnc openssl-devel
+firewall-cmd --permanent --add-port=3389/tcp
+firewall-cmd --permanent --add-port=3389/udp
+firewall-cmd --reload
